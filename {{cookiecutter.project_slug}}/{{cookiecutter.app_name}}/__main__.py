93de035dac2d0ec91c4632d044442c42140e19bf
DUSTI-JOHNSON\dusti <dustijohnson@outlook.com>
2022-09-24 21:00:31 -0600

    dot env

--- {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/__main__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.app_name}}/__main__.py
@@ -7,17 +7,17 @@ from .cli import get_cli_args
 from .core.config import config
 from .core.logger import logger
 
+# Setup config and logger using cli args
+cli_args = get_cli_args()
+logger.start(cli_args.loglevel)
+config.load(cli_args.config_path)
+
 
 def main():
     """Execute the application."""
     logger.info("Launching program.")
     raise NotImplementedError
 
-
-# Setup config and logger using cli args
-cli_args = get_cli_args()
-logger.start(cli_args.loglevel)
-config.load(cli_args.config_path)
 
 if __name__ == "__main__":
     raise SystemExit(main())
